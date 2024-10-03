62b4e3054f3efb7f517831f3c1bd3946f0fb482d
홍종훈 <whdgnszz1@snu.ac.kr>
2024-10-03 13:10:22 +0900

    번들러 최적화 적용 완료

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,71 @@
-import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
     },
   },
+
+  build: {
+    target: 'es2015',
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  server: {
+    compress: true,
+  },
+  sourcemap: process.env.NODE_ENV === 'development',
 });
